3e9a90ca6eba50781d2b5cf68f29a8113ae7f92e
Rehan <rehan99000@gmail.com>
2020-10-05 17:37:23 +0500

    fix indentation

--- ecommerce/static/js/utils/subscription_utils.js
+++ ecommerce/static/js/utils/subscription_utils.js
@@ -1,34 +1,30 @@
-define([
-	'backbone',
-	'js-cookie',
-    'underscore'
-], function(
-    Backbone,
-    Cookies,
-    _
+define(['backbone', 'js-cookie', 'underscore'], function (
+	Backbone,
+	Cookies,
+	_
 ) {
-    'use strict';
+	'use strict';
 
-    return {
-        /**
-         * Returns a subscription type text corresponding to the subscription type slug.
-         *
-         * @param {String} subscriptionType
-         * @returns {String}
-         */
-        formatSubscriptionType: function(subscriptionType) {
-            if (subscriptionType === 'limited-access')
-                return gettext('Limited Access');
-            else if (subscriptionType === 'full-access-courses')
-                return gettext('Full Access (Courses)');
-            else if (subscriptionType === 'full-access-time-period')
-                return gettext('Full Access (Time Period)');
-            else if (subscriptionType === 'lifetime-access')
-                return gettext('Lifetime Access');
-            return '';
-        },
+	return {
+		/**
+		 * Returns a subscription type text corresponding to the subscription type slug.
+		 *
+		 * @param {String} subscriptionType
+		 * @returns {String}
+		 */
+		formatSubscriptionType: function (subscriptionType) {
+			if (subscriptionType === 'limited-access')
+				return gettext('Limited Access');
+			else if (subscriptionType === 'full-access-courses')
+				return gettext('Full Access (Courses)');
+			else if (subscriptionType === 'full-access-time-period')
+				return gettext('Full Access (Time Period)');
+			else if (subscriptionType === 'lifetime-access')
+				return gettext('Lifetime Access');
+			return '';
+		},
 
-        setCoursePaymentsButtonText: function() {
+		setCoursePaymentsButtonText: function () {
 			Backbone.ajax({
 				type: 'GET',
 				headers: {
@@ -37,13 +33,18 @@ define([
 				url:
 					window.location.origin +
 					'/api/v2/subscriptions/course_payments_status/',
-				success: function(data) {
-                    var coursePaymentsButton = $('[name=course-payments]');
-                    if (data.course_payments)
-                        coursePaymentsButton.text(gettext('Disable Course Payments'));
-                    else coursePaymentsButton.text(gettext('Enable Course Payments'));
-                },
+				success: function (data) {
+					var coursePaymentsButton = $('[name=course-payments]');
+					if (data.course_payments)
+						coursePaymentsButton.text(
+							gettext('Disable Course Payments')
+						);
+					else
+						coursePaymentsButton.text(
+							gettext('Enable Course Payments')
+						);
+				},
 			});
-        }
-    }
-})
+		},
+	};
+});
