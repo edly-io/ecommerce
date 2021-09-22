1b4fe944cec103beb3c4a3069fd15ab670e574a0
Asad Manzoor <asad33manzoor@gmail.com>
2021-09-22 16:39:12 +0500

    Rename session info API to CSRF API

--- ecommerce/extensions/edly_ecommerce_app/api/v1/urls.py
+++ ecommerce/extensions/edly_ecommerce_app/api/v1/urls.py
@@ -6,6 +6,6 @@ from ecommerce.extensions.edly_ecommerce_app.api.v1 import views
 app_name = 'v1'
 urlpatterns = [
     url(r'site_themes/', views.SiteThemesActions.as_view(), name='site_themes'),
-    url(r'session_info/', views.UserSessionInfo.as_view(), name='get_user_session_info'),
+    url(r'csrf_token/', views.CSRFTokenInfo.as_view(), name='get_csrf_token'),
     url(r'edly_sites/', views.EdlySiteViewSet.as_view(), name='edly_sites'),
 ]
